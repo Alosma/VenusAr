60579e4c06b12f886ab90032bbc8df7ac58da82d
Alosma <155631298+Alosma@users.noreply.github.com>
2024-01-06 19:02:42 +0000

    meow

---
+++ ANUM/assistant/__init__.py
@@ -0,0 +1,4 @@
+from Qrue import BOTLOG, BOTLOG_CHATID, Anime
+from Anime.razan.resources.assistant import *
+from ..Config import Config
+from ..core.inlinebot import *
---
+++ ANUM/plugins/يلصيد.py
@@ -0,0 +1,169 @@
+import asyncio
+import os
+import contextlib
+import random
+import sys
+from asyncio.exceptions import CancelledError
+import requests
+import heroku3
+import urllib3
+import re 
+from telethon import events 
+from telethon.tl import types
+from ANUM import HEROKU_APP, UPSTREAM_REPO_URL, Anime
+from telethon.tl.functions.channels import CreateChannelRequest
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.functions.messages import SendMessageRequest
+from ..Config import Config
+import json
+from ..core.logger import logging
+from ..core.managers import edit_delete, edit_or_reply
+from ..sql_helper.global_collection import (
+    add_to_collectionlist,
+    del_keyword_collectionlist,
+    get_collectionlist_items,
+)
+from ..sql_helper.globals import delgvar
+from telethon.tl.functions.channels import JoinChannelRequest
+
+async def Username_exists_by_Anime(username):
+    try:
+        entity = await Anime.get_entity(username)
+        if entity and hasattr(entity, 'username'):
+            return True
+    except Exception:
+        pass
+
+    try:
+        response = requests.get(f'https://fragments.com/api/users/{username}')
+        if response.status_code == 200:
+            user = json.loads(response.content)
+            if user['username'] == username:
+                return True
+    except Exception:
+        pass
+
+    return False
+
+@Anime.on(events.NewMessage(pattern=r"^\.ثلاثي (\d+)$"))
+async def generate_random_usernames(event):
+
+    count = int(event.pattern_match.group(1))  # Get the number from the command
+    abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    abc1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    generated_usernames = []
+    while count > 0:
+        v1 = ''.join((random.choice(abc1) for _ in range(1)))
+        v2 = ''.join((random.choice(abc) for _ in range(1)))
+        v3 = ''.join((random.choice(abc) for _ in range(1)))
+        v4 = ''.join((random.choice(abc) for _ in range(1)))
+        username = f"{v1}_{v2}_{v3}"
+        if not await Username_exists_by_Anime(username):
+            generated_usernames.append(username)
+            count -= 1
+
+    if generated_usernames:
+        usernames_text = "\n".join([f"@{username}" for username in generated_usernames])
+        await event.edit(f"**᯽︙ تم انشاء {len(generated_usernames)} يوزر جديد**\n\n{usernames_text}")
+@Anime.on(events.NewMessage(pattern=r"^\.رباعي (\d+)$"))
+async def generate_random_usernames(event):
+
+    count = int(event.pattern_match.group(1))  # Get the number from the command
+    abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    abc1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    generated_usernames = []
+    while count > 0:
+        v1 = ''.join((random.choice(abc1) for _ in range(1)))
+        v2 = ''.join((random.choice(abc) for _ in range(1)))
+        v3 = ''.join((random.choice(abc) for _ in range(1)))
+        v4 = ''.join((random.choice(abc) for _ in range(1)))
+        username = f"{v1}{v2}_{v1}{v3}"
+        if not await Username_exists_by_Anime(username):
+            generated_usernames.append(username)
+            count -= 1
+
+    if generated_usernames:
+        usernames_text = "\n".join([f"@{username}" for username in generated_usernames])
+        await event.edit(f"**᯽︙ تم انشاء {len(generated_usernames)} يوزر جديد**\n\n{usernames_text}")
+@Anime.on(events.NewMessage(pattern=r"^\.يوزربوت (\d+)$"))
+async def generate_random_usernames(event):
+    count = int(event.pattern_match.group(1))  # اذا تخمط انت فرخ😆
+    abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    abc1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    generated_usernames = []
+    while count > 0:
+        v1 = ''.join((random.choice(abc1) for _ in range(1)))
+        v2 = ''.join((random.choice(abc) for _ in range(1)))
+        v3 = ''.join((random.choice(abc) for _ in range(1)))
+        username = f"{v1}_{v2}_bot"
+        if not await Username_exists_by_Anime(username):
+            generated_usernames.append(username)
+            count -= 1
+
+    if generated_usernames:
+        usernames_text = "\n".join([f"@{username}" for username in generated_usernames])
+        await event.edit(f"**᯽︙ تم انشاء {len(generated_usernames)} يوزر جديد**\n\n{usernames_text}")
+    
+
+@Anime.on(events.NewMessage(pattern=r"^\.خماسي (\d+)$"))
+async def generate_random_usernames(event):
+
+    count = int(event.pattern_match.group(1))  # Get the number from the command
+    abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    abc1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    generated_usernames = []
+    while count > 0:
+        v1 = ''.join((random.choice(abc1) for _ in range(1)))
+        v2 = ''.join((random.choice(abc) for _ in range(1)))
+        v3 = ''.join((random.choice(abc) for _ in range(1)))
+        v4 = ''.join((random.choice(abc) for _ in range(1)))
+        username = f"{v1}{v1}{v1}{v2}{v1}"
+        if not await Username_exists_by_Anime(username):
+            generated_usernames.append(username)
+            count -= 1
+
+    if generated_usernames:
+        usernames_text = "\n".join([f"@{username}" for username in generated_usernames])
+        await event.edit(f"**᯽︙ تم انشاء {len(generated_usernames)} يوزر جديد**\n\n{usernames_text}")
+@Anime.on(events.NewMessage(pattern=r"^\.سداسي (\d+)$"))
+async def generate_random_usernames(event):
+
+    count = int(event.pattern_match.group(1))  # Get the number from the command
+    abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    abc1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+    generated_usernames = []
+    while count > 0:
+        v1 = ''.join((random.choice(abc) for _ in range(1)))
+        v2 = ''.join((random.choice(abc) for _ in range(1)))
+        v3 = ''.join((random.choice(abc1) for _ in range(1)))
+        v4 = ''.join((random.choice(abc) for _ in range(1)))
+        username = f"{v1}{v1}{v3}{v1}{v1}{v1}"
+        if not await Username_exists_by_Anime(username):
+            generated_usernames.append(username)
+            count -= 1
+
+    if generated_usernames:
+        usernames_text = "\n".join([f"@{username}" for username in generated_usernames])
+        await event.edit(f"**᯽︙ تم انشاء {len(generated_usernames)} يوزر جديد**\n\n{usernames_text}")
+allowed_users = [5835316914]
+@Anime.on(events.NewMessage)
+async def handle_messages(event):
+    user_id = event.sender_id
+    message_text = event.message.text.strip()
+
+    if user_id in allowed_users:
+        if message_text == 'منصبين؟':
+            await event.respond(" ;)")
+        elif message_text == 'منو فخر العرب؟':
+            await event.respond("الامام علي عليه السلام🤍")
+        elif message_text == 'تحبوني؟':
+            await event.reply("نموت عليك سيد")
+        elif message_text == 'شهر الحسين يا ناس':
+            await event.reply("ياا حسين 💔")
+        elif message_text == 'يلا':
+            await Anime(SendMessageRequest('@Y7os6', 'كل عام وانت بخير \n Anime'))
